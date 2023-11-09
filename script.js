5d4b377aba65c420c775108e69ea6c93240b1dc6
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-09 19:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+	let arr = str.slice('');
+	let arr2 = [];
+	for(let i = arr.length-1; i >= 0; i--){
+		arr2.push(arr[i]);
+	}
 
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i] != arr2[i]){
+			return `${str} is not a palindrome`;
+		}
+	}
+			return `${str} is a palindrome`;
 }
 module.exports = palindrome
